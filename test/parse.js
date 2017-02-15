ac7274f5052fbb17b56722a945c61aa39cf0536a
Matt Neary <neary.matt@gmail.com>
2017-02-15 00:54:16 -0500

    better tests

--- test/parse.js
+++ test/parse.js
@@ -2,18 +2,21 @@ import test from 'ava'
 
 import {parse, stringify} from '..'
 
-const parses = (t, expr) => {
-  t.is(expr, stringify(parse(expr)))
+const parses = (t, expr, expected) => {
+  t.is(expected || expr, stringify(parse(expr)))
 }
 parses.title = expr => `parses ${expr}`
 
-test('parse', parses, 'A (1 + (2 * 3) , "ABC")')
+test('parse', parses, 'A(1 + (2 * 3), "ABC")', 'A (1 + (2 * 3) , "ABC")')
 test('parse', parses, '1 + 2 * 3')
 test('parse', parses, '1 + 2 * 3 * 4 + 2')
-test('parse', parses, 'A + 2 ; 1')
+test('parse', parses, 'A + 2; 1', 'A + 2 ; 1')
 test('parse', parses, `
+ A + 2
+ 1
+`, `
  A + 2 
  1 
 `)
-test('parse', parses, '[A , 1 , "ABC"]')
-test('parse', parses, '{a : 1 , b : 2}')
+test('parse', parses, '[A, 1, "ABC"]', '[A , 1 , "ABC"]')
+test('parse', parses, '{a: 1, b: 2}', '{a : 1 , b : 2}')
